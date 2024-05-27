603c3b687a9a5eaf740c27c5cd4c7516e56cbe94
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-05-27 15:29:18 +0300

    Update and rename *09.Magic Matrices.js to 09.Magic Matrices.js

---
+++ Arrays and Nested Arrays/09.Magic Matrices.js
@@ -0,0 +1,19 @@
+function task(arrs){
+    let isMagical = true
+    let finalSum = 0
+
+    for(let i = 0; i < arrs.length; i++){
+        let sum = 0
+        let currentRow = arrs[i]
+        for(let j = 0; j < arrs.length; j++){
+            sum += currentRow[j]
+        }
+        if(i === 0){
+            finalSum = sum
+        }else if(sum !== finalSum){
+            isMagical = false
+        }
+    }
+
+    console.log(isMagical)
+}
