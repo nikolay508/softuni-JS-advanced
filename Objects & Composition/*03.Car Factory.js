5b3907d59e032d8a2eef1b1e7e3e00a2cd78bfb1
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-01 00:26:19 +0300

    Update *03.Car Factory.js

--- Objects & Composition/*03.Car Factory.js
+++ Objects & Composition/*03.Car Factory.js
@@ -1,30 +1,13 @@
 function task(obj){
-    let result = {};
-    result.model = obj.model;
+    let result = {model: obj.model, engine: undefined, carriage: undefined, wheels: undefined};
 
     let wantedEngine = obj.power;
     if(wantedEngine <= 90){
         result.engine = {power: 90, volume: 1800};
-    }else if(wantedEngine >= 240){
-        result.engine = {power: 240, volume: 3500};
-    }else if(wantedEngine === 120){
+    }else if(wantedEngine <= 120){
         result.engine = {power: 120, volume: 2400};
-    }else if(wantedEngine > 90 && wantedEngine < 120){
-        let sum1 = (90 - wantedEngine) * -1;
-        let sum2 = 120 - wantedEngine;
-        if(sum1 <= sum2){
-            result.engine = {power: 90, volume: 1800};
-        }else if(sum1 > sum2){
-            result.engine = {power: 120, volume: 2400};
-        }
-    }else if(wantedEngine > 120 && wantedEngine < 240){
-        let sum1 = (120 - wantedEngine) * -1;
-        let sum2 = 240 - wantedEngine;
-        if(sum1 <= sum2){
-            result.engine = {power: 120, volume: 2400};
-        }else if(sum1 > sum2){
-            result.engine = {power: 240, volume: 3500};
-        }
+    }else{
+        result.engine = {power: 200, volume: 3500};
     }
 
     result.carriage = {type: obj.carriage, color: obj.color};
@@ -33,7 +16,8 @@ function task(obj){
     if(wheel % 2 === 0){
         wheel -= 1;
     }
-    result.wheelsize = [wheel, wheel, wheel, wheel];
+    result.wheels = new Array(4).fill(wheel);
 
+    console.log(result);
     return result;
 }
