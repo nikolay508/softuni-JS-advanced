59939ac2a890f55b14256f90badc4a4ef4ab43f9
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-05-31 22:06:01 +0300

    Update 02.Construction Crew.js

--- Objects & Composition/02.Construction Crew.js
+++ Objects & Composition/02.Construction Crew.js
@@ -2,7 +2,6 @@ function task(obj){
     if(obj.dizziness === true){
         let weightkg = Number(obj.weight);
         let experienceYears = Number(obj.experience);
-
         let sum = 0.1 * weightkg * experienceYears;
         obj.levelOfHydrated = sum;
         obj.dizziness = false;
