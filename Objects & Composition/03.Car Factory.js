c8da349bbb8a9eed923bd7b2b85619c973f8f14c
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-01 13:46:34 +0300

    Update 03.Car Factory.js

--- Objects & Composition/03.Car Factory.js
+++ Objects & Composition/03.Car Factory.js
@@ -1,22 +1,20 @@
 function task(obj){
-    let result = {model: obj.model, engine: undefined, carriage: undefined, wheels: undefined};
-
-    let wantedEngine = obj.power;
-    if(wantedEngine <= 90){
-        result.engine = {power: 90, volume: 1800};
-    }else if(wantedEngine <= 120){
-        result.engine = {power: 120, volume: 2400};
+    let final = { model: obj.model, engine: {}, carriage: {}, wheels: [] };
+    
+    if(obj.power <= 90){
+        final.engine = { power: 90, volume: 1800 };
+    }else if(obj.power <= 120){
+        final.engine = { power: 120, volume: 2400 };
     }else{
-        result.engine = {power: 200, volume: 3500};
-    }
+        final.engine = { power: 200, volume: 3500 };
+    };
 
-    result.carriage = {type: obj.carriage, color: obj.color};
+    final.carriage = { type: obj.carriage, color: obj.color };
 
-    let wheel = Number(obj.wheelsize);
-    if(wheel % 2 === 0){
-        wheel -= 1;
-    }
-    result.wheels = new Array(4).fill(wheel);
+    if(obj.wheelsize % 2 === 0){
+        obj.wheelsize = obj.wheelsize - 1;
+    };
+    final.wheels = new Array(4).fill(obj.wheelsize);
 
-    return result;
+    return final;
 }
