e03fa9d36e7bc3ed0882ca4655c0d3d3b158a8ad
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-01 17:38:33 +0300

    Update 06.Store Catalogue.js

--- Objects & Composition/06.Store Catalogue.js
+++ Objects & Composition/06.Store Catalogue.js
@@ -1,23 +1,22 @@
 function task(arr){
     let final = [];
+
     for(const str of arr){
         let [product, price] = str.split(' : ');
         price = Number(price);
         final.push({product, price});
     }
     let sorted = final.sort((a, b) => (a.product).localeCompare(b.product));
-    
-    let currentLetter = sorted[0].product[0];
-    console.log(currentLetter);
-    for(let i = 0; i < sorted.length; i++){
-        let firstLetter = sorted[i].product[0];
 
-        if(firstLetter === currentLetter){
-            console.log(`  ${sorted[i].product}: ${sorted[i].price}`);
+    let currentLetter = '';
+    for(let i = 0; i < final.length; i++){
+        let letter = final[i].product[0];
+        if(currentLetter === letter){
+            console.log(`  ${final[i].product}: ${final[i].price}`);
         }else{
-            currentLetter = firstLetter;
+            currentLetter = letter;
             console.log(currentLetter);
-            console.log(`  ${sorted[i].product}: ${sorted[i].price}`);
+            console.log(`  ${final[i].product}: ${final[i].price}`);
         }
     }
 }
