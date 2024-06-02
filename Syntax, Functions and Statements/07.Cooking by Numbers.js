394c17aed6b059ab940ce7302603d7307ed77781
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-02 23:13:10 +0300

    Update 07.Cooking by Numbers.js

--- Syntax, Functions and Statements/07.Cooking by Numbers.js
+++ Syntax, Functions and Statements/07.Cooking by Numbers.js
@@ -1,23 +1,23 @@
 function task(strNum, command1, command2, command3, command4, command5){
-    let arr = [command1, command2, command3, command4, command5]
-    let number = Number(strNum)
+    let arr = [command1, command2, command3, command4, command5];
+    let number = Number(strNum);
 
     for(let i = 0; i < arr.length; i++){
         if(arr[i] === 'chop'){
-            number /= 2
-            console.log(number)
+            number /= 2;
+            console.log(number);
         }else if(arr[i] === 'dice'){
-            number = Math.sqrt(number)
-            console.log(number)
+            number = Math.sqrt(number);
+            console.log(number);
         }else if(arr[i] === 'spice'){
-            number += 1
-            console.log(number)
+            number += 1;
+            console.log(number);
         }else if(arr[i] === 'bake'){
-            number *= 3
+            number *= 3;
             console.log(number)
         }else if(arr[i] === 'fillet'){
-            number -= (0.2 * number)
-            console.log(number)
+            number -= (0.2 * number);
+            console.log(number);
         }
     }
 }
