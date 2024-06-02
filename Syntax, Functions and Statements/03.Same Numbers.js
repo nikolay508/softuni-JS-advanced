d495ca6d933b0ecb10341d7d63e60bc2a502b602
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-02 23:11:03 +0300

    Update 03.Same Numbers.js

--- Syntax, Functions and Statements/03.Same Numbers.js
+++ Syntax, Functions and Statements/03.Same Numbers.js
@@ -1,20 +1,20 @@
 function task(num){
-    let isValid = true
-    let sum = 0
-    let number = String(num)
+    let isValid = true;
+    let sum = 0;
+    let number = String(num);
 
     for(let i = 0; i < number.length; i++){
-        let digit = Number(number[i])
-        let nextDigit = Number(number[i+1])
-        sum += digit
+        let digit = Number(number[i]);
+        let nextDigit = Number(number[i+1]);
+        sum += digit;
         if(digit !== nextDigit && i !== number.length-1){
-            isValid = false
+            isValid = false;
         }
     }
     if(isValid){
-        console.log('true')
+        console.log('true');
     }else{
-        console.log('false')
+        console.log('false');
     }
-    console.log(sum)
+    console.log(sum);
 }
