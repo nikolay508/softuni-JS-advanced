d9b3d80a42d7f6092c36c56886b9dd7caaf4552c
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-02 23:11:56 +0300

    Update 05.Time to Walk.js

--- Syntax, Functions and Statements/05.Time to Walk.js
+++ Syntax, Functions and Statements/05.Time to Walk.js
@@ -1,11 +1,11 @@
 function task(steps, footprint, speedKM){
-    let distanceKM = (steps * footprint)/1000
-    let brakes = Math.floor((distanceKM*1000) / 500)
-    let hours = distanceKM / speedKM
-    let minutes = (hours * 60) + brakes
-    let seconds = ((minutes - Math.floor(minutes)) * 60).toFixed()
-    let finalHour = Math.floor(hours)
-    let finalMinute = Math.floor(minutes)
-    let finalSecond = Math.floor(seconds)
-    console.log(`${String(finalHour).padStart(2, '0')}:${String(finalMinute).padStart(2, '0')}:${String(finalSecond).padStart(2, '0')}`)
+    let distanceKM = (steps * footprint)/1000;
+    let brakes = Math.floor((distanceKM*1000) / 500);
+    let hours = distanceKM / speedKM;
+    let minutes = (hours * 60) + brakes;
+    let seconds = ((minutes - Math.floor(minutes)) * 60).toFixed();
+    let finalHour = Math.floor(hours);
+    let finalMinute = Math.floor(minutes);
+    let finalSecond = Math.floor(seconds);
+    console.log(`${String(finalHour).padStart(2, '0')}:${String(finalMinute).padStart(2, '0')}:${String(finalSecond).padStart(2, '0')}`);
 }
