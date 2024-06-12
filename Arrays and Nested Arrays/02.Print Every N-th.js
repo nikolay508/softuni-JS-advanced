f693fee173407d6126e7b365311f55f17676822b
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-12 21:56:20 +0300

    Update 02.Print Every N-th.js

--- Arrays and Nested Arrays/02.Print Every N-th.js
+++ Arrays and Nested Arrays/02.Print Every N-th.js
@@ -1,12 +1,12 @@
 function task(arr, N){
-    let final = [arr[0]]
-    let count = 0
+    let final = [arr[0]];
+    let count = 0;
     for(let i = 1; i < arr.length; i++){
-        count++
+        count++;
         if(count === N){
-            count = 0
-            final.push(arr[i])
+            count = 0;
+            final.push(arr[i]);
         }
     }
-    return final
+    return final;
 }
