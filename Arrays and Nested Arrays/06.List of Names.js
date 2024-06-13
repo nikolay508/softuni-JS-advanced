8afbd74eddbd92091e81fcd9ee3a5448b7bf3b07
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-13 12:27:37 +0300

    Update 06.List of Names.js

--- Arrays and Nested Arrays/06.List of Names.js
+++ Arrays and Nested Arrays/06.List of Names.js
@@ -1,10 +1,10 @@
 function task(arr){
     let sorted = arr.sort((a, b) => {
-        return a.localeCompare(b)})
-    let count = 0
+        return a.localeCompare(b)});
+    let count = 0;
 
     for(let i = 0; i < sorted.length; i++){
-        count++
-        console.log(`${count}.${sorted[i]}`)
+        count++;
+        console.log(`${count}.${sorted[i]}`);
     }
 }
