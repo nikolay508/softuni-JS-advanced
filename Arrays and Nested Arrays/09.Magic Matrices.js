9cc8f0d6f0cd7e7fd2b31e227335310d76fb1426
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-13 12:29:00 +0300

    Update 09.Magic Matrices.js

--- Arrays and Nested Arrays/09.Magic Matrices.js
+++ Arrays and Nested Arrays/09.Magic Matrices.js
@@ -1,19 +1,19 @@
 function task(arrs){
-    let isMagical = true
-    let finalSum = 0
+    let isMagical = true;
+    let finalSum = 0;
 
     for(let i = 0; i < arrs.length; i++){
-        let sum = 0
-        let currentRow = arrs[i]
+        let sum = 0;
+        let currentRow = arrs[i];
         for(let j = 0; j < arrs.length; j++){
-            sum += currentRow[j]
+            sum += currentRow[j];
         }
         if(i === 0){
-            finalSum = sum
+            finalSum = sum;
         }else if(sum !== finalSum){
-            isMagical = false
+            isMagical = false;
         }
     }
 
-    console.log(isMagical)
+    console.log(isMagical);
 }
