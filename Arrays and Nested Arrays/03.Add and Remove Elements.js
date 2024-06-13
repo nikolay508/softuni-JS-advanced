b710e36f0096360df816e400efd4a32e92b7c1a6
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-13 12:26:32 +0300

    Update 03.Add and Remove Elements.js

--- Arrays and Nested Arrays/03.Add and Remove Elements.js
+++ Arrays and Nested Arrays/03.Add and Remove Elements.js
@@ -1,19 +1,19 @@
 function task(arr){
-    let final = []
-    let number = 1
+    let final = [];
+    let number = 1;
     
     for(command of arr){
         if(command === 'add'){
-            final.push(number)
+            final.push(number);
         }else if(command === 'remove'){
-            final.pop()
+            final.pop();
         }
-        number++
+        number++;
     }
 
     if(final.length > 0){
-        console.log(final.join('\n'))
+        console.log(final.join('\n'));
     }else{
-        console.log('Empty')
+        console.log('Empty');
     }
 }
