ec2970c8e0aad79c5f52f71018b0a3f847ae847c
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-20 23:22:55 +0300

    Create 03. Fibonacci.js

---
+++ Advanced Function/03. Fibonacci.js
@@ -0,0 +1,14 @@
+function getFibonator(){
+    let arrNums = [0, 1]
+    let count = 0;
+    return () => {
+        count++;
+        if(count === 1){
+            return 1;
+        }else{
+            let num = (arrNums[count - 1] + arrNums[count - 2]);
+            arrNums.push(num);
+            return num;
+        }
+    };
+}
