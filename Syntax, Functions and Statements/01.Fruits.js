ba897916632670d64b4bffe38b2206706b88d847
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-21 17:21:31 +0300

    Update 01.Fruits.js

--- Syntax, Functions and Statements/01.Fruits.js
+++ Syntax, Functions and Statements/01.Fruits.js
@@ -1,5 +1,6 @@
 function fruits(fruit, weightGrams, priceKG){
     let weightKG = weightGrams / 1000;
     let money = priceKG * weightKG;
+    
     console.log(`I need $${money.toFixed(2)} to buy ${weightKG.toFixed(2)} kilograms ${fruit}.`);
 }
