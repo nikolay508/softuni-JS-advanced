95e0ddad15b557d1463ce25a4f5e542eb920805b
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-23 15:49:13 +0300

    Create 02. Argument Info.js

---
+++ Advanced Functions/02. Argument Info.js
@@ -0,0 +1,19 @@
+function task(...all){
+    let obj = {}
+    for(each of all){
+        let type = typeof(each);
+        if(obj.hasOwnProperty(type)){
+            obj[type] += 1;
+        }else{
+            obj[type] = 1;
+        }
+        console.log(`${type}: ${each}`);
+    }
+    
+    let sorted = Object.entries(obj).sort((a, b) => b[1] - a[1]);
+    for(each of sorted){
+        if(each[1] > 0){
+            console.log(`${each[0]} = ${each[1]}`);
+        }
+    }
+}
