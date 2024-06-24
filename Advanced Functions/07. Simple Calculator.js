3830d927aa329d2e3d7732c4b2f9df5b83ad3872
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-06-24 14:42:37 +0300

    Create 07. Simple Calculator.js

---
+++ Advanced Functions/07. Simple Calculator.js
@@ -0,0 +1,19 @@
+function calculator() {
+    let obj = {};
+    return {
+        init: (a, b, c) => {
+            obj.resultArea = document.querySelector(c);
+            obj.selector1 = document.querySelector(a); obj.selector2 = document.querySelector(b);
+        },
+        add: () => {
+            let num1 = Number(obj.selector1.value);
+            let num2 = Number(obj.selector2.value);
+            obj.resultArea.value = num1 + num2
+        },
+        subtract: () => {
+            let num1 = Number(obj.selector1.value);
+            let num2 = Number(obj.selector2.value);
+            obj.resultArea.value = num1 - num2
+        }
+    }
+}
