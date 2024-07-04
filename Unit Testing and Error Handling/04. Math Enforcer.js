8ce9b482bdf12ffe41321f20016f8103cb06e82b
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-07-04 14:54:30 +0300

    Create 04. Math Enforcer.js

---
+++ Unit Testing and Error Handling/04. Math Enforcer.js
@@ -0,0 +1,97 @@
+describe('maths', ()=>{
+    describe('addFive', ()=>{
+        it('should return undefined', ()=>{
+            expect(mathEnforcer.addFive('string')).to.equal(undefined);
+        })
+        it('should return undefined', ()=>{
+            expect(mathEnforcer.addFive(true)).to.equal(undefined);
+        })
+        it('should return undefined', ()=>{
+            expect(mathEnforcer.addFive({name: 'hey'})).to.equal(undefined);
+        })
+        it('should return undefined', ()=>{
+            expect(mathEnforcer.addFive([1, 2])).to.equal(undefined);
+        })
+
+        it('should return number', ()=>{
+            expect(mathEnforcer.addFive(5)).to.equal(10);
+        })
+        it('should return number', ()=>{
+            expect(mathEnforcer.addFive(5.5)).to.equal(10.5);
+        })
+        it('should return number', ()=>{
+            expect(mathEnforcer.addFive(0)).to.equal(5);
+        })
+        it('should return number', ()=>{
+            expect(mathEnforcer.addFive(-5)).to.equal(0);
+        })
+    })
+
+    describe('subtractTen', ()=>{
+        it('should return undefined', ()=>{
+            expect(mathEnforcer.subtractTen('string')).to.equal(undefined);
+        })
+        it('should return undefined', ()=>{
+            expect(mathEnforcer.subtractTen(true)).to.equal(undefined);
+        })
+        it('should return undefined', ()=>{
+            expect(mathEnforcer.subtractTen({name: 'hey'})).to.equal(undefined);
+        })
+        it('should return undefined', ()=>{
+            expect(mathEnforcer.subtractTen([1, 2])).to.equal(undefined);
+        })
+
+        it('should return zero', ()=>{
+            expect(mathEnforcer.subtractTen(10)).to.equal(0);
+        })
+        it('should return negative number', ()=>{
+            expect(mathEnforcer.subtractTen(0)).to.equal(-10);
+        })
+        it('should return float number', ()=>{
+            expect(mathEnforcer.subtractTen(10.5)).to.equal(0.5);
+        })
+        it('should return negative number', ()=>{
+            expect(mathEnforcer.subtractTen(-10)).to.equal(-20);
+        })
+    })
+
+    describe('sum', ()=>{
+        it('should return undefined', ()=>{
+            expect(mathEnforcer.sum('string', false)).to.equal(undefined);
+        })
+        it('should return undefined', ()=>{
+            expect(mathEnforcer.sum(true, 'string')).to.equal(undefined);
+        })
+        it('should return undefined', ()=>{
+            expect(mathEnforcer.sum({name: 'hey'}, [3, 4])).to.equal(undefined);
+        })
+        it('should return undefined', ()=>{
+            expect(mathEnforcer.sum([1, 2], {name: 'hey'})).to.equal(undefined);
+        })
+        it('should return undefined', ()=>{
+            expect(mathEnforcer.sum(2, true)).to.equal(undefined);
+        })
+        it('should return undefined', ()=>{
+            expect(mathEnforcer.sum([1, 2], 5.5)).to.equal(undefined);
+        })
+
+        it('should return number', ()=>{
+            expect(mathEnforcer.sum(2, 3)).to.equal(5);
+        })
+        it('should return number', ()=>{
+            expect(mathEnforcer.sum(0, 5)).to.equal(5);
+        })
+        it('should return number', ()=>{
+            expect(mathEnforcer.sum(-2, 3)).to.equal(1);
+        })
+        it('should return zero', ()=>{
+            expect(mathEnforcer.sum(0, 0)).to.equal(0);
+        })
+        it('should return negative number', ()=>{
+            expect(mathEnforcer.sum(-2, -3)).to.equal(-5);
+        })
+        it('should return negative float umber', ()=>{
+            expect(mathEnforcer.sum(2.5, 2.3)).to.equal(4.8);
+        })
+    })
+})
