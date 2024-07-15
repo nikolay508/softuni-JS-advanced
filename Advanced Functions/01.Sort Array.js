d2380a65b4f91da81708262753d974c0b2bca44a
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-07-15 14:15:39 +0300

    Update 01.Sort Array.js

--- Advanced Functions/01.Sort Array.js
+++ Advanced Functions/01.Sort Array.js
@@ -1,9 +1,11 @@
 function task(arr, criteria){
     let sorted = [];
+    
     if(criteria === 'asc'){
         sorted = arr.sort((a,b) => {return a-b});
     }else if(criteria === 'desc'){
         sorted = arr.sort((a,b) => {return b-a});
     }
+    
     return sorted;
 }
