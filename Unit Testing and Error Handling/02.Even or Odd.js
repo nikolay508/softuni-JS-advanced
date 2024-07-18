89434dfdad0dad2d2bf852084cc9beb2b5a60589
Nikolay Todorov <157305036+nikolay508@users.noreply.github.com>
2024-07-19 01:11:32 +0300

    Update 02.Even or Odd.js

--- Unit Testing and Error Handling/02.Even or Odd.js
+++ Unit Testing and Error Handling/02.Even or Odd.js
@@ -1,22 +1,22 @@
 describe('String', ()=>{
     
-    it('should be undefined', ()=>{
+    it('should be undefined', () => {
         expect(isOddOrEven(12)).to.equal(undefined);
     })
 
-    it('should be undefined', ()=>{
+    it('should be undefined', () => {
         expect(isOddOrEven(true)).to.equal(undefined);
     })
 
-    it('should be even', ()=>{
+    it('should be even', () => {
         expect(isOddOrEven('math')).to.equal('even');
     })
 
-    it('should be odd', ()=>{
+    it('should be odd', () => {
         expect(isOddOrEven('maths')).to.equal('odd');
     })
 
-    it('should be odd', ()=>{
+    it('should be odd', () => {
         expect(isOddOrEven('maths', 'heyy', 'wassup')).to.equal('odd');
     })
     
