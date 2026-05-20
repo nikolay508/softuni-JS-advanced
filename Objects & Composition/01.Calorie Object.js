8c1334f5370901d6d48ee5d31671e6d87c213f01
Nikolay Todorov <nikolay.b.todorov@gmail.com>
2026-05-21 02:16:50 +0300

    Update 01.Calorie Object.js

--- Objects & Composition/01.Calorie Object.js
+++ Objects & Composition/01.Calorie Object.js
@@ -1,11 +1,11 @@
-function task(arr){
-    let obj = {};
+function calorieObject(arr) {
+    const result = {};
     
-    for(let i = 0; i < arr.length; i++){
-        if(i % 2 === 0){
-            obj[arr[i]] = Number(arr[i+1]);
-        }
+    for (let i = 0; i < arr.length; i += 2) {
+        const name = arr[i];
+        const calories = Number(arr[i + 1]);
+        result[name] = calories;
     }
     
-    console.log(obj);
+    console.log(result);
 }
