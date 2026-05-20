a06724a2ba1efa7d31793a4a97070425dadb07b9
Nikolay Todorov <nikolay.b.todorov@gmail.com>
2026-05-21 02:18:43 +0300

    Update 07.Towns to JSON.js

--- Objects & Composition/07.Towns to JSON.js
+++ Objects & Composition/07.Towns to JSON.js
@@ -1,12 +1,15 @@
 function townsToJSON(arr) {
     const final = [];
+    
     for (let i = 1; i < arr.length; i++) {
         let [, town, latitude, longitude] = arr[i].split('|');
+        
         final.push({
             Town: town.trim(),
             Latitude: JSON.parse(Number(latitude).toFixed(2)),
             Longitude: JSON.parse(Number(longitude).toFixed(2))
         });
     }
+    
     console.log(JSON.stringify(final));
 }
